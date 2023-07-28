c38bf17be7c0564850d3eb880569738ab7b9135c
Satyam800 <satyamchandra7277@gmail.com>
2023-07-28 12:46:55 +0530

    Added to the backend and dend the form data

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -1,48 +1,77 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Form = () => {
+  const [form, setform] = useState({});
+  const [Data, SetData] = useState([]);
+  const [value,Setvalue]= useState(false)
+  const handleform = (e) => {
+    setform({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const [form,setform]=useState({})
-    const handleform=(e)=>{
-
-        setform({
-            ...form,
-            [e.target.name]:e.target.value
-            })         
-    }
-
-     const handleSubmit=async (e)=>{
-        e.preventDefault()
-       const response= await fetch('http://localhost:8000/',{
-        method:'POST',
-        body:JSON.stringify(form),
-        headers:{
-            "content-type":"application/json"
-        }
-       })
-       const result= await response.json()
-       console.log(result);
-
-
-     }
-  return (
-   <>
-   
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     
-    <form onSubmit={handleSubmit}>
 
-        <p>{JSON.stringify(form)}</p>
+    const response = await fetch("http://localhost:2000/data", {
+      method: "POST",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    });
+    const result = await response.json();
+    console.log(result);
+    Setvalue(true)
+  };
+
+  const getData = async () => {
+    let data = await fetch("http://localhost:2000/getdata", {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+        Authorization: "Bearer YOUR_ACCESS_TOKEN",
+      },
+    });
+    let finaldata = await data.json();
+    SetData(...finaldata);
+    console.log(finaldata);
+    Setvalue(false)
+  };
+
+  useEffect(() => {
+    console.log(form, "form");
+    getData();
+  }, [value]);
+  
+
+  
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
         <span>Usename</span>
-        <input placeholder='username' name="username" type="text" onChange={handleform}/>
+        <input
+          placeholder="username"
+          name="username"
+          type="text"
+          onChange={handleform}
+        />
         <span>Password</span>
-        <input placeholder='passowrd' name="password" type="text" onChange={handleform}/>
+        <input
+          placeholder="passowrd"
+          name="password"
+          type="text"
+          onChange={handleform}
+        />
         <input type="submit"></input>
+      </form>
+    </>
+  );
+};
 
-    </form>
-   
-   
-   </>
-  )
-}
-
-export default Form
+export default Form;
